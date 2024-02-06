cd5f6f99b157ede4485cbb5c107208abc5958bca
Dike Kingsley <125186879+Pure-coder007@users.noreply.github.com>
2024-02-06 17:12:19 +0100

    commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room
 
 
 app = Flask(__name__)
@@ -22,10 +22,19 @@ def chat():
         return redirect(url_for('home'))
 
 
+
+
+@socketio.on('send_message')
+def handle_send_message_event(data):
+    app.logger.info("{} has sent message the room {}: {}".format(data['username'], data['room'], data['message']))
+    socketio.emit('receive_message', data, room=data['room'])
+
+
 @socketio.on('join_room')
 def handle_join_room_event(data):
     app.logger.info("{} has joined the room {}".format(data['username'], data['room']))
-
+    join_room(data['room'])
+    socketio.emit('join_room_announcement', data, room=data['room'])
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
